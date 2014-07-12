9f671c62898a74dd6ca16cb834d6011b1e5d1a62
svb2357 <svanbeek2357@gmail.com>
2014-07-12 18:53:16 +0100

    First commit.

---
+++ seg.py
@@ -0,0 +1,20 @@
+class City():
+	def __init__(self):
+		pass
+
+	def generate():
+		pass
+
+	def printMap():
+		pass
+
+	def Update():
+		pass
+
+
+class Person():
+	def __init__(self):
+		pass
+
+	def calcHappiness(self):
+		pass
